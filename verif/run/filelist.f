29ab4125a93bc859a68d9c7064f633dcaecc2b1d
dinesha <dinesha@39499b2a-a18e-4c0b-b8b8-223e3a536977>
2012-01-17 11:59:49 +0000

    Run files are updated into SVN

---
+++ verif/run/filelist.f
@@ -0,0 +1,11 @@
++define+S50 \
++incdir+../../rtl/core \
+../tb/tb.sv \
+../model/IS42VM16400K.V
+../model/mt48lc2m32b2.v \
+../../rtl/core/sdrc_core.v \
+../../rtl/core/sdrc_bank_ctl.v \
+../../rtl/core/sdrc_bank_fsm.v \
+../../rtl/core/sdrc_bs_convert.v \
+../../rtl/core/sdrc_req_gen.v \
+../../rtl/core/sdrc_xfr_ctl.v 
